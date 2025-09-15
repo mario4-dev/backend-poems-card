1d31966fb5d686279dbad50c62768d98ea4c0a29
mario4-dev <isnp671219@ugb.edu.sv>
2025-09-15 11:27:29 -0600

    add all fixed

--- database/migrations/2025_09_12_152222_create_poems_table.php
+++ database/migrations/2025_09_12_152222_create_poems_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('poems', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('title');
             $table->text('content');
             $table->string('author');
