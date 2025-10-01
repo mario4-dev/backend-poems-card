f37fbc7cf0a1249ea3b961ec7b0acb6212cf5868
mario4-dev <isnp671219@ugb.edu.sv>
2025-10-01 14:14:09 -0600

    Adding user HAHAH

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,14 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            UserSeeder::class,
+            PoemSeeder::class,
         ]);
-
-        // Ejecutar el seeder de poemas
-        $this->call(PoemSeeder::class);
     }
 }
